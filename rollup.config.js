ded2fac5f44cdf116af1c8770e1d4837100fb93d
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-01 07:13:28 +0530

    add changes

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugin: [dts()],
+        plugins: [dts()],
     }
 
 
